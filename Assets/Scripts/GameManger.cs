860e804725afc32ea5f154f7b4c20047816a86b4
b02902092 <b02902092@ntu.edu.tw>
2017-11-21 00:20:13 +0800

    more bugs are revised

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManger : MonoBehaviour {
-    private Client client;
+    public Client client;
     // Use this for initialization
     void Start()
     {
