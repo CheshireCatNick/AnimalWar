f22dc4836e38f9f254c18e6a55966a38e1fe818e
B02705034 <b02705034@ntu.edu.tw>
2017-11-21 18:36:00 +0800

    playermove插值移動

--- Assets/Scripts/Playermove.cs
+++ Assets/Scripts/Playermove.cs
@@ -6,52 +6,36 @@ public class Playermove: MonoBehaviour {
 
     Rigidbody2D playerRigidbody2D;
 
-    //傳進Destination
+    public Vector2 Destination;
 
+    Vector2 INITDESTINATION = new Vector2(1000,1000);
+    const string HORIZONTAL = "Horizontal";
 
-    [Header("水平速度")]
-    public float speedX;
-    [Header("垂直速度")]
-    public float speedY;
-    [Header("終點座標")]
-    public Vector2 Destinaiton;
-    [Header("目前水平方向")]
-    public float horizontalDir;//-1~1之間
+    public void Move()  {
+        //獲得當前位置
+        Vector2 curenPosition = playerRigidbody2D.transform.position;
+        
+        if (Destination != INITDESTINATION) {
+            float speed = 150;
 
-    const string HORIZONTAL = "Horizontal" ;
-
-    [Header("水平推力")]
-    [Range(0,500)]
-    public float xForce;
-
-    [Header("最大水平速度")]
-    public float maxSpeedX;
-
-    public void ControlSpeed(){
-        speedX =  playerRigidbody2D.velocity.x;
-        speedY =  playerRigidbody2D.velocity.y;
-        float newSpeedX = Mathf.Clamp(speedX, -maxSpeedX, maxSpeedX);
-        playerRigidbody2D.velocity = new Vector2(newSpeedX, speedY);
-    }
-
-    
-
-
-    /// <summary>水平移動</summary>
-    void MovementX () {
-        playerRigidbody2D = GetComponent<Rigidbody2D>();
-        horizontalDir = Destinaiton.x - playerRigidbody2D.position.x;
-        while ( playerRigidbody2D.position.x != Destinaiton.x ) {
-            playerRigidbody2D.AddForce(new Vector2(xForce * horizontalDir, 0));
+            if (Vector2.Distance(curenPosition, Destination) < 0.01f)
+            {
+                transform.position = Destination;
+            }
+            else
+            {
+                //插值移動
+                //距離就等於 間隔時間乘以速度
+                float maxDistanceDelta = Time.deltaTime * speed;
+                transform.position = Vector2.MoveTowards(curenPosition, Destination, maxDistanceDelta);
+            }
         }
     }
 
+
 	// Update is called once per frame
 	void Update () {
-        MovementX();
-        //目前速度
-        speedX = playerRigidbody2D.velocity.x;
-        ControlSpeed();
-
+        Move();
+        
 	}
 }
