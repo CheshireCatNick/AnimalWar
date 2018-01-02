e93f543e2939ce65c12b67967bbcde5bbaceb196
LIAGM <louis19950117@yahoo.com.tw>
2018-01-03 04:41:12 +0800

    test

---
+++ Assets/Scripts/Destroy_ray.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_ray : MonoBehaviour
+{
+    public GameObject ray;
+    // Use this for initialization
+    void Update()
+    {
+        Destroy(ray);
+    }
+}
--- Assets/Scripts/HPControl.cs
+++ Assets/Scripts/HPControl.cs
@@ -6,19 +6,18 @@ public class HPControl : MonoBehaviour
 {
 
     private float m_hp;
-    private Text m_text;
+    public Text m_text;
 
     // Use this for initialization
     void Start()
     {
-        m_text = GetComponent<Text>();
         m_hp = Health.Instance.GetHP();
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_text.text ="HP : "+ m_hp.ToString();
         m_hp = Health.Instance.GetHP();
+        m_text.text ="HP : "+ m_hp.ToString();
     }
 }
