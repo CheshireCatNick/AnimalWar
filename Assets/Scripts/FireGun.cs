143324e249e07b21df322eda4cdf6326b5b64619
賴廷威 <laitingwei@laitingweideMacBook-Pro.local>
2018-01-03 21:31:46 +0800

    firegun bug fixed

--- Assets/Scripts/FireGun.cs
+++ Assets/Scripts/FireGun.cs
@@ -14,18 +14,17 @@ public class FireGun : MonoBehaviour {
     // Update is called once per frame
     public void Shoot(Vector2 target)
     {
-        float x = target.x - transform.position.x;
+		float x = (target.x - transform.position.x) * this.transform.parent.localScale.x;
         float y = target.y - transform.position.y;
+
         angle = Mathf.Atan2(y , x);//得到砲管角度		
         angle *= (180/Mathf.PI);
         //angle = Mathf.Clamp (angle, minAngle, maxAngle);
         Vector3 temp = firegun.transform.localEulerAngles;
         temp.z = angle;
         firegun.transform.localEulerAngles = temp;//上下旋轉砲管
-
         GameObject shoot = (GameObject)Instantiate(Cannonball, firePoint.position, firePoint.rotation);
         shoot.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(speed, 0));
-        //Physics.IgnoreCollision (transform.root.GetComponent<Collider> (), shoot.GetComponent<Collider> ());
-        
+        Physics2D.IgnoreCollision (transform.root.GetComponent<Collider2D> (), shoot.GetComponent<Collider2D> ());
     }
 }
