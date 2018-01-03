9990d937f5403223e18ae5136de186490d584ba9
Louis Tsai(蔡侑儒) <Louis_Tsai@HTC.COM>
2018-01-04 04:11:25 +0800

    add Main scene

---
+++ Assets/Scripts/JumpScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpScene : MonoBehaviour {
+
+    public void JumpSceneEvent(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
