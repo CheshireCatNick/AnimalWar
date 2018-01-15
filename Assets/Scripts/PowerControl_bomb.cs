54d9d5b7d59e495b8980ae75452cca041c648630
LIAGM <louis19950117@yahoo.com.tw>
2018-01-16 00:00:34 +0800

    add power to targets and delete something

---
+++ Assets/Scripts/PowerControl_bomb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerControl_bomb : MonoBehaviour {
+
+
+    private int r = 3;
+    private int m_power;
+	private Vector3 start_point;
+    public TextMesh m_text;
+
+    // Use this for initialization
+    void Start()
+    {
+		start_point = this.transform.position;
+        m_power = 0;
+        m_text.text = m_power.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float power_x = this.transform.position.x - start_point.x;
+        float power_y = this.transform.position.y - start_point.y;
+        m_power = (int)(100 * (Mathf.Sqrt((power_x*power_x)+(power_y*power_y))/Mathf.Sqrt(r*r)));
+        if(m_power >= 100)
+            m_power = 100;
+        m_text.text =m_power.ToString();
+    }
+}
