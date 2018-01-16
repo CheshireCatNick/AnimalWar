6522d248f8e1d627fa63f5e457dab0e04ddf5617
Louis Tsai(蔡侑儒) <louis19950117@yahoo.com.tw>
2018-01-16 10:37:43 +0800

    modify power

--- Assets/Scripts/PowerControl_bomb.cs
+++ Assets/Scripts/PowerControl_bomb.cs
@@ -8,43 +8,19 @@ public class PowerControl_bomb : MonoBehaviour {
 
     private int r = 3;
     private int m_power;
-	private Vector3 start_point;
-    private Vector3 end_point;
-    private bool isCalled;
+    public Vector3 start_point;
+    public Vector3 end_point;
+    private float power_x;
+    private float power_y;
     public TextMesh m_text;
 
-    // Use this for initialization
-    void Start()
-    {
-		start_point = this.transform.position;
-        m_power = 0;
-        m_text.text = m_power.ToString();
-        isCalled = false;
-    }
-    public void Update_power(Vector3 start, Vector3 end)
-    {
-        start_point = start;
-        end_point = end;
-        isCalled = true;
-    }
-    // Update is called once per frame
     void Update()
     {
-        float power_x;
-        float power_y;
-        if (!isCalled)
-        {
-            power_x = this.transform.position.x - start_point.x;
-            power_y = this.transform.position.y - start_point.y;
-        }
-        else
-        {
-            power_x = end_point.x - start_point.x;
-            power_y = end_point.y - start_point.y;
-        }
-        m_power = (int)(100 * (Mathf.Sqrt((power_x*power_x)+(power_y*power_y))/Mathf.Sqrt(r*r)));
-        if(m_power >= 100)
+        power_x = end_point.x - start_point.x;
+        power_y = end_point.y - start_point.y;
+        m_power = (int)(100 * (Mathf.Sqrt((power_x * power_x) + (power_y * power_y)) / Mathf.Sqrt(r * r)));
+        if (m_power >= 100)
             m_power = 100;
-        m_text.text =m_power.ToString();
+        m_text.text = m_power.ToString();
     }
 }
